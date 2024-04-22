40bace4c6d77db01524d27fcbd47df8d37f50ed7
Thang Phan <phancaothng@gmail.com>
2024-04-22 14:10:42 +0300

    feat: add medusa-payment-stripe plugin

--- medusa-config.js
+++ medusa-config.js
@@ -1,46 +1,48 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-let ENV_FILE_NAME = '';
+let ENV_FILE_NAME = "";
 switch (process.env.NODE_ENV) {
-  case 'production':
-    ENV_FILE_NAME = '.env.production';
+  case "production":
+    ENV_FILE_NAME = ".env.production";
     break;
-  case 'staging':
-    ENV_FILE_NAME = '.env.staging';
+  case "staging":
+    ENV_FILE_NAME = ".env.staging";
     break;
-  case 'test':
-    ENV_FILE_NAME = '.env.test';
+  case "test":
+    ENV_FILE_NAME = ".env.test";
     break;
-  case 'development':
+  case "development":
   default:
-    ENV_FILE_NAME = '.env';
+    ENV_FILE_NAME = ".env";
     break;
 }
 
 try {
-  dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
+  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
 } catch (e) {}
 
 // CORS when consuming Medusa from admin
-const ADMIN_CORS = process.env.ADMIN_CORS || 'http://localhost:7000,http://localhost:7001';
+const ADMIN_CORS =
+  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || 'http://localhost:8000';
+const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
 
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost/medusa-starter-default';
+const DATABASE_URL =
+  process.env.DATABASE_URL || "postgres://localhost/medusa-starter-default";
 
-const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379';
+const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
-const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:9000';
-const ADMIN_URL = process.env.ADMIN_URL || 'http://localhost:7000';
-const STOREFRONT_URL = process.env.STOREFRONT_URL || 'http://localhost:8000';
+const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:9000";
+const ADMIN_URL = process.env.ADMIN_URL || "http://localhost:7000";
+const STOREFRONT_URL = process.env.STOREFRONT_URL || "http://localhost:8000";
 
 // Social Auth
-const GoogleClientId = process.env.GOOGLE_CLIENT_ID || '';
-const GoogleClientSecret = process.env.GOOGLE_CLIENT_SECRET || '';
+const GoogleClientId = process.env.GOOGLE_CLIENT_ID || "";
+const GoogleClientSecret = process.env.GOOGLE_CLIENT_SECRET || "";
 
-const FacebookClientId = process.env.FACEBOOK_CLIENT_ID || '';
-const FacebookClientSecret = process.env.FACEBOOK_CLIENT_SECRET || '';
+const FacebookClientId = process.env.FACEBOOK_CLIENT_ID || "";
+const FacebookClientSecret = process.env.FACEBOOK_CLIENT_SECRET || "";
 
 const plugins = [
   `medusa-fulfillment-manual`,
@@ -48,30 +50,30 @@ const plugins = [
   {
     resolve: `@medusajs/file-local`,
     options: {
-      upload_dir: 'uploads',
+      upload_dir: "uploads",
     },
   },
   {
-    resolve: '@medusajs/admin',
+    resolve: "@medusajs/admin",
     /** @type {import('@medusajs/admin').PluginOptions} */
     options: {
       autoRebuild: true,
       develop: {
-        open: process.env.OPEN_BROWSER !== 'false',
+        open: process.env.OPEN_BROWSER !== "false",
       },
     },
   },
   {
-    resolve: '@groccia/medusa-postcode-plugin',
+    resolve: "@groccia/medusa-postcode-plugin",
   },
   {
-    resolve: 'medusa-plugin-auth',
+    resolve: "medusa-plugin-auth",
     /** @type {import('medusa-plugin-auth').AuthOptions} */
     options: [
       {
-        type: 'google',
-        strict: 'none',
-        identifier: 'google',
+        type: "google",
+        strict: "none",
+        identifier: "google",
         clientID: GoogleClientId,
         clientSecret: GoogleClientSecret,
         admin: {
@@ -86,9 +88,9 @@ const plugins = [
         },
       },
       {
-        type: 'facebook',
-        strict: 'none',
-        identifier: 'facebook',
+        type: "facebook",
+        strict: "none",
+        identifier: "facebook",
         clientID: FacebookClientId,
         clientSecret: FacebookClientSecret,
         admin: {
@@ -104,17 +106,24 @@ const plugins = [
       },
     ],
   },
+  {
+    resolve: `medusa-payment-stripe`,
+    options: {
+      api_key: process.env.STRIPE_API_KEY,
+      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
+    },
+  },
 ];
 
 const modules = {
   eventBus: {
-    resolve: '@medusajs/event-bus-redis',
+    resolve: "@medusajs/event-bus-redis",
     options: {
       redisUrl: REDIS_URL,
     },
   },
   cacheService: {
-    resolve: '@medusajs/cache-redis',
+    resolve: "@medusajs/cache-redis",
     options: {
       redisUrl: REDIS_URL,
     },
